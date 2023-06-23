50f6d5674976a2efe87e0de67ea208129392bb0f
joyliz <chepkoechjoyliz@gmail.com>
2023-06-23 14:59:46 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
